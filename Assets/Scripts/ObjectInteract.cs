132a41c9fd9b80e08a33238e4ad921489d4a7483
gabz-cdp <gabz.cdp@gmail.com>
2026-09-18 11:10:47 +0200

    Implemented new interaction system

--- Assets/Scripts/ObjectInteract.cs
+++ Assets/Scripts/ObjectInteract.cs
@@ -3,135 +3,141 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Device;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
 public class ObjectInteract : MonoBehaviour
 {
-    public GameObject Offset;
-    private PlayerInput _playerInput;
-    GameObject targetObject;
+    public GameObject offset; //where the object will be held infront of the player
+    public GameObject player;
+    private PlayerInput playerInput; //how the player interacts with the surrounding
+    private GameObject targetObject;
 
-    public bool isExamining = false;
-    public Canvas _canva;
-    public GameObject tableObject;
-    private Vector3 lastMousePosition;
-    private Transform examinedObject;
+    public bool isExamining = false; //checks to see that the obejct is being held and examined
 
-    private Dictionary<Transform, Vector3> originalPositions = new Dictionary<Transform, Vector3>();
-    private Dictionary<Transform, Quaternion> originalRotations = new Dictionary<Transform, Quaternion>();
+    public Canvas objectInteractCanvas; //canvas used to interact and rotate object 
+    public Canvas interactMenu;
+    public Canvas HUDCanvas; //main canvas
+    public GameObject tableObject; //where the clinder is placed on the table
+    private Vector3 lastMousePosition; //checks where the mouse was positioned
+    private Transform examinedObject; //used to move the object that is being examined
+    private Vector3 mousePosition; //current mouse position
+    public bool isHitting; //checks to see that the raycast is hitting the object being examined
+
+    private Rect screenArea = new Rect(UnityEngine.Screen.width / 2 - 600, UnityEngine.Screen.height / 2 - 375, 1200, 750);
+
+    //List of position & rotation of interactable objects
+    public Dictionary<Transform, Vector3> originalPositions = new Dictionary<Transform, Vector3>();
+    public Dictionary<Transform, Quaternion> originalRotations = new Dictionary<Transform, Quaternion>();
 
     void Start()
     {
-        _canva.enabled = false;
+        objectInteractCanvas.enabled = false;
+        interactMenu.enabled = false;
+        HUDCanvas.enabled = true;
         targetObject = GameObject.Find("Player");
-        _playerInput = targetObject.GetComponent<PlayerInput>();
+        playerInput = targetObject.GetComponent<PlayerInput>();
     }
 
     void Update()
     {
-        /*if(context.performed)
+        mousePosition = Mouse.current.position.ReadValue();
+        float distance = Vector3.Distance(targetObject.transform.position, tableObject.transform.position);
+        //offset.transform.forward = -player.transform.forward;
+
+        if (Keyboard.current.eKey.wasPressedThisFrame) //Rchecks to see that the E key was pressed to interact
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = default;
+            Ray interactRay = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit interactHit;
 
-            if (hit.collider.CompareTag("Object"))
+            if (Physics.Raycast(interactRay, out interactHit))
             {
-                ToggleExamination();
-
-                if(isExamining)
+                if (interactHit.collider.CompareTag("Object"))
                 {
-                    examinedObject = hit.transform;
-                    originalPositions[examinedObject] = examinedObject.position;
-                    originalRotations[examinedObject] = examinedObject.rotation;
+                    if (distance < 10f)
+                    {
+                        isExamining = true;
+
+                        if (isExamining) //stores the object being examined and its original position & rotation
+                        {
+                            examinedObject = interactHit.transform;
+                            originalPositions[examinedObject] = examinedObject.position;
+                            originalRotations[examinedObject] = examinedObject.rotation;
+                            objectInteractCanvas.enabled = false;
+                            interactMenu.enabled = true;
+                            HUDCanvas.enabled = false;
+                            Examine(); StartExamination();
+                        }
+                        else
+                        {
+                            objectInteractCanvas.enabled = true;
+                            interactMenu.enabled = false;
+                            HUDCanvas.enabled = true;
+                            NonExamine(); StopExamination();
+                        }
+                    }
                 }
             }
-        }*/
-
-        if(CheckUserClose())
-        {
-            if (isExamining)
-            {
-                _canva.enabled = false;
-                Examine();
-                StartExamination();
-            }
-            else
-            {
-                _canva.enabled = true;
-                NonExamine();
-                StopExamination();
-            }
         }
-        else _canva.enabled = false;
-                                 
     }
 
-    public void ToggleExamination()
-    {
-        isExamining = !isExamining;
+    public void ExitButtonPressed()
+    { 
+        isExamining = false;
     }
 
+    //When a player starts examining an object, unlocks cursor, makes it visible and stops player from moving
     void StartExamination()
     {
-        lastMousePosition = Input.mousePosition;
+        lastMousePosition = mousePosition;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-        _playerInput.enabled = false;
+        playerInput.enabled = false;
     }
 
+    //When the player stops examing an object, it locks the cursor, hides it, and allows the player to move
     void StopExamination()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        _playerInput.enabled = true;
+        playerInput.enabled = true;
     }
 
+    //Moves the object picked up (examinedObject) to the offset so the player can rotate the object based on the movement of the mouse
     void Examine()
     {
-        if (examinedObject != null)
+        if(examinedObject != null)
         {
-            examinedObject.position = Vector3.Lerp(examinedObject.position, Offset.transform.position, 0.2f);
-
-            Vector3 deltaMouse = Input.mousePosition - lastMousePosition;
-            float rotationSpeed = 1.0f;
-            examinedObject.Rotate(deltaMouse.x * rotationSpeed * Vector3.up, Space.World);
-            examinedObject.Rotate(deltaMouse.y * rotationSpeed * Vector3.left, Space.World);
-            lastMousePosition = Input.mousePosition;
+            if(screenArea.Contains(mousePosition))
+            {
+                examinedObject.position = Vector3.Lerp(examinedObject.position, offset.transform.position, 0.2f);
+                if(Mouse.current.leftButton.isPressed)
+                {
+                    Vector3 deltaMouse = mousePosition - lastMousePosition;
+                    float rotationSpeed = 1.0f;
+                    examinedObject.Rotate(deltaMouse.x * rotationSpeed * Vector3.up, Space.World);
+                    examinedObject.Rotate(deltaMouse.y * rotationSpeed * Vector3.left, Space.World);
+                    lastMousePosition = mousePosition;
+                }
+            }
         }
     }
 
-    //This method is called when the player is not examining an object.
-    //It resets the position and rotation of the examined object to its original values stored in the dictionaries.
-
+    //When the player is no longer examining the object, returns the object to original position and rotation
     void NonExamine()
     {
-        if (examinedObject != null)
+        if(examinedObject != null)
         {
-            // Reset the position and rotation of the examined object to its original values
-            if (originalPositions.ContainsKey(examinedObject))
+            if(originalPositions.ContainsKey(examinedObject)) //resets position & rotation of object to original values
             {
                 examinedObject.position = Vector3.Lerp(examinedObject.position, originalPositions[examinedObject], 0.2f);
             }
-            if (originalRotations.ContainsKey(examinedObject))
+            if(originalRotations.ContainsKey(examinedObject))
             {
                 examinedObject.rotation = Quaternion.Slerp(examinedObject.rotation, originalRotations[examinedObject], 0.2f);
             }
         }
     }
-
-
-    // This method calculates the distance between the player(targetObject) and 
-    // an object called tableObject.If the distance is less than 2 units, it returns true, indicating that the player is close to the object.
-    bool CheckUserClose()
-    {
-        // Calculate the distance between the two GameObjects
-        float distance = Vector3.Distance(targetObject.transform.position, tableObject.transform.position);
-
-        // Check if they are close based on the threshold
-        return (distance < 2);
-
-    }
-
 }
